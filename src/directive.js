d35e4b8419c6eeb95c4618cab63d32d8100803ec
zhaocjcc <40732782+zhaocjcc@users.noreply.github.com>
2019-04-09 11:54:17 +0800

    Update directive.js

--- src/directive.js
+++ src/directive.js
@@ -2,7 +2,7 @@
 export default {
   // 之所以用 inserted 而不是 bind，是需要确保 contentmenu mounted 后才进行 addRef 操作
   inserted (el, binding, vnode) {
-    const contextmenu = vnode.context.$refs[binding.arg]
+    const contextmenu = vnode.context.$refs[binding.value]
 
     contextmenu.addRef({ el, vnode })
     contextmenu.$contextmenuId = el.id || contextmenu._uid // eslint-disable-line no-underscore-dangle
